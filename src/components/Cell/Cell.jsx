faabf0e3fa683e6a6f10ceeae8babe340863cba9
Mészáros Lajos <m_lajos@hotmail.com>
2021-10-19 23:31:18 +0200

    Most már lehet 'rajzolni' a Grid-re

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
 import cn from 'classnames'
-import { useDispatch } from 'react-redux'
+import { noop } from 'ramda-adjunct'
 import s from './style.module.scss'
 
-const Cell = ({ isAlive, x, y }) => {
-  const dispatch = useDispatch()
-
-  // TODO: lehessen rajzolni, ne csak kattintani
-
-  const toggleCell = (x, y) => {
-    // TODO: ha már elindult a szimuláció, akkor már ne lehessen módosítani
-    dispatch({
-      type: 'Game.toggleCell',
-      payload: {
-        x,
-        y
-      }
-    })
-  }
-
-  return (
-    <div
-      className={cn(s.Cell, { [s.alive]: isAlive })}
-      onClick={() => {
-        toggleCell(x, y)
-      }}
-    />
-  )
+const Cell = ({ isAlive, onMouseEnter = noop, onMouseDown = noop, onMouseUp = noop }) => {
+  return <div className={cn(s.Cell, { [s.alive]: isAlive })} {...{ onMouseEnter, onMouseDown, onMouseUp }} />
 }
 
 export default Cell
--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,21 +1,65 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { getGridSize } from '../../helpers/functions.js'
 import Cell from '../Cell/Cell.jsx'
 import s from './style.module.scss'
 
 const Grid = () => {
+  const dispatch = useDispatch()
+
   const grid = useSelector((state) => state.Game.grid)
   const { width, height } = useSelector((state) => getGridSize(state.Game.grid))
+
+  const [isMouseDown, setIsMouseDown] = useState(false)
+  const [mousePos, setMousePos] = useState({ x: null, y: null })
+
+  const toggleCell = (x, y) => {
+    // TODO: ha már elindult a szimuláció, akkor már ne lehessen módosítani
+    dispatch({
+      type: 'Game.toggleCell',
+      payload: {
+        x,
+        y
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (mousePos.x !== null && mousePos.y !== null) {
+      toggleCell(mousePos.x, mousePos.y)
+    }
+  }, [mousePos.x, mousePos.y])
 
   return (
     <div
       className={s.Grid}
       style={{ gridTemplateColumns: `repeat(${width}, 1fr)`, gridTemplateRows: `repeat(${height}, 1fr)` }}
+      onMouseLeave={() => {
+        setIsMouseDown(false)
+      }}
     >
       {grid.map((row, y) => {
         return row.map((cell, x) => {
-          return <Cell key={`${x}:${y}`} isAlive={cell === 1} x={x} y={y} />
+          return (
+            <Cell
+              key={`${x}:${y}`}
+              isAlive={cell === 1}
+              onMouseDown={() => {
+                setIsMouseDown(true)
+                setMousePos({ x, y })
+              }}
+              onMouseUp={() => {
+                setIsMouseDown(false)
+              }}
+              onMouseEnter={() => {
+                if (isMouseDown) {
+                  if (mousePos.x !== x || mousePos.y !== y) {
+                    setMousePos({ x, y })
+                  }
+                }
+              }}
+            />
+          )
         })
       })}
     </div>
