66079063919efcb28abf720a1e077bb3fb24840f
Mészáros Lajos <m_lajos@hotmail.com>
2021-10-19 20:21:11 +0200

    Tettem be TODO-kat magamnak

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -6,7 +6,10 @@ import s from './style.module.scss'
 const Cell = ({ isAlive, x, y }) => {
   const dispatch = useDispatch()
 
+  // TODO: lehessen rajzolni, ne csak kattintani
+
   const toggleCell = (x, y) => {
+    // TODO: ha már elindult a szimuláció, akkor már ne lehessen módosítani
     dispatch({
       type: 'Game.toggleCell',
       payload: {
